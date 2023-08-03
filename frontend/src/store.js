05a4949ef8abb13e25dda817d3587184f6bbd37c
LAPTOP-GD7ICC6K <sbsangu1412gmail.com>
2023-08-04 03:19:36 +0530

    bug fixes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { taskReducer } from "./components/Reducers/task";
 
-export const server=`http://localhost:8000/api/v1`
+export const server=`https://task-manager-five-vert.vercel.app/api/v1`
 const store=configureStore({
  reducer:{
      task:taskReducer
