be0ad8ef20e32dd051e87ced10a4cd82d0fcbfa0
LAPTOP-GD7ICC6K <sbsangu1412gmail.com>
2023-08-04 02:54:55 +0530

    bug fixes

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,4 @@ app.get("/", (req, res) =>
 );
 
 
-app.listen(PORT,()=>(
- console.log(`server is running on ${PORT}`)
-
-));
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import app from "./app/js"
+
+
+app.listen(PORT,()=>(
+ console.log(`server is running on ${PORT}`)
+
+));
